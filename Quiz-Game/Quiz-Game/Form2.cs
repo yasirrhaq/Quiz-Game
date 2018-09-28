3d837ee316cfa71c2f6d944cf58eb28ec0163a5b
MuhWildanShalahuddin <wildanshalahuddin@gmail.com>
2018-09-28 15:38:39 +0700

    Revert "nambahin array jawaban + otomatis ganti option setiap pertanyaan bedasarkan array"
    This reverts commit 51370278a091393a9ec5fec7b724dbbfc89aa2e0.

--- Quiz-Game/Quiz-Game/Form2.cs
+++ Quiz-Game/Quiz-Game/Form2.cs
@@ -10,85 +10,65 @@ using System.Windows.Forms;
 
 namespace Quiz_Game
 {
-
+   
     public partial class Form2 : Form
     {
-        static int counter;
-        string[] naskah = new string[] { "siapa ayah hamo?", "kenapa dipanggil hamo ?", "apa itu hamo?" };
-        string[] option1 = new string[] { "gak tau", "sulit nih", "mager jawab" };
-        string[] option2 = new string[] { "hm apa ya", "kepo lu", "C mungkin?" };
-        string[] option3 = new string[] { "sumpah gatau", "apa urusan anda bertanya?", "Yang bener B kok" };
-        string[] option4 = new string[] { "nyerah bang", "jawab sndiri aja", "Arrg gatau" };
+<<<<<<< HEAD
+         static int counter = 1;
+        string[] naskah = new string[] 
+        { "siapa ayah hamo?", "kenapa dipanggil hamo ?" , "apa itu hamo?"};
+=======
+         static int counter;
+        string []naskah = new string[] { "siapa ayah hamo?", "kenapa dipanggil hamo ?" , "apa itu hamo?"};
+>>>>>>> f1e18b3a3fa1ead46ffa78ded3db6c124000fc5f
+
 
 
 
 
         public Form2()
         {
+
             InitializeComponent();
+           
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
-            button1.Text = option1[counter];
-            button2.Text = option2[counter];
-            button3.Text = option3[counter];
-            button4.Text = option4[counter];
-            string a = naskah[counter];
-            soal.Text = a.ToString();
+
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (counter < 2)
-            {
-                counter++;
-                button1.Text = option1[counter];
-                button2.Text = option2[counter];
-                button3.Text = option3[counter];
-                button4.Text = option4[counter];
-                string a = naskah[counter];
-                soal.Text = a.ToString();
-            }
+            string a = naskah[counter];
+            counter = counter + 1;
+            soal.Text = a.ToString();
+
+
+
         }
+
         private void button2_Click(object sender, EventArgs e)
         {
-            if (counter < 2)
-            {
-                counter++;
-                button1.Text = option1[counter];
-                button2.Text = option2[counter];
-                button3.Text = option3[counter];
-                button4.Text = option4[counter];
-                string a = naskah[counter];
-                soal.Text = a.ToString();
-            }
+            string a = naskah[counter];
+            counter = counter + 1;
+            soal.Text = a.ToString();
         }
+
         private void button3_Click(object sender, EventArgs e)
         {
-            if (counter < 2)
-            {
-                counter++;
-                button1.Text = option1[counter];
-                button2.Text = option2[counter];
-                button3.Text = option3[counter];
-                button4.Text = option4[counter];
-                string a = naskah[counter];
-                soal.Text = a.ToString();
-            }
+            string a = naskah[counter];
+            counter = counter + 1;
+            soal.Text = a.ToString();
         }
+
         private void button4_Click(object sender, EventArgs e)
         {
-            if (counter < 2)
-            {
-                counter++;
-                button1.Text = option1[counter];
-                button2.Text = option2[counter];
-                button3.Text = option3[counter];
-                button4.Text = option4[counter];
-                string a = naskah[counter];
-                soal.Text = a.ToString();
-            }
+            string a = naskah[counter];
+            counter = counter + 1;
+            soal.Text = a.ToString();
+
         }
     }
 }
