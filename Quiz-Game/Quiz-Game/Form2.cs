605974dda2c268449baf6eb5853634e526e92e61
MuhWildanShalahuddin <wildanshalahuddin@gmail.com>
2018-10-01 14:00:30 +0700

    berhasil konek database

--- Quiz-Game/Quiz-Game/Form2.cs
+++ Quiz-Game/Quiz-Game/Form2.cs
@@ -7,35 +7,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Quiz_Game
 {
    
     public partial class Form2 : Form
     {
-<<<<<<< HEAD
-         static int counter = 1;
+        static int counter = 1;
         string[] naskah = new string[] 
         { "siapa ayah hamo?", "kenapa dipanggil hamo ?" , "apa itu hamo?"};
-=======
-         static int counter;
-        string []naskah = new string[] { "siapa ayah hamo?", "kenapa dipanggil hamo ?" , "apa itu hamo?"};
->>>>>>> f1e18b3a3fa1ead46ffa78ded3db6c124000fc5f
 
 
-
+        public MySqlConnection myCon;
 
 
         public Form2()
         {
 
             InitializeComponent();
-           
+
+            string conStr = "datasource=localhost; port=3306; username=root; password=12345;database=ppk;SslMode=none";
+            myCon = new MySqlConnection(conStr);
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            string query = "select*from soal";
+            MySqlDataReader myReader; 
 
+            try
+            {
+                myCon.Open();
+                MySqlCommand myCommand = new MySqlCommand(query, myCon);
+
+                myReader = myCommand.ExecuteReader();
+                StringBuilder sb = new StringBuilder();
+
+                while(myReader.Read())
+                {
+                    sb.Append("soal : "+myReader[1]);
+                }
+
+                myReader.Close();
+                myCon.Close();
+                MessageBox.Show(sb.ToString());
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
             
         }
 
@@ -69,6 +90,20 @@ namespace Quiz_Game
             counter = counter + 1;
             soal.Text = a.ToString();
 
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myCon.Open();
+                MessageBox.Show("koneksi berhasil");
+                myCon.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
