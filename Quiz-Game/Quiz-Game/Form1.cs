bc1f6346facc85d27b1e612f65c7fa708de05bfa
Yasir Haq <yasir.haq98@gmail.com>
2018-10-02 18:52:10 +0700

    Insert username into database

--- Quiz-Game/Quiz-Game/Form1.cs
+++ Quiz-Game/Quiz-Game/Form1.cs
@@ -7,14 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Quiz_Game
 {
     public partial class Form1 : Form
     {
+        private MySqlConnection myCon;
+
         public Form1()
         {
             InitializeComponent();
+            string conStr = "datasource=sql12.freemysqlhosting.net; port=3306; username=sql12259336; password=K2cckElyBj;database=sql12259336;SslMode=none";
+            myCon = new MySqlConnection(conStr);
         }
 
         private void buttonStart_Click(object sender, EventArgs e)
@@ -25,6 +30,18 @@ namespace Quiz_Game
             }
             else
             {
+                try
+                {
+                    myCon.Open();
+                    MySqlCommand myCommand = new MySqlCommand("insert into player(username) values ('"+textBox1.Text+"');", myCon);
+                    myCommand.ExecuteNonQuery();
+                    MessageBox.Show(textBox1.Text);
+                    myCon.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
                 Form2 f2 = new Form2();
                 this.Hide();
                 f2.Show();
